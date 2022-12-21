3a3a2e2087dd0212f63dfca25025a11b596e3823
franco <sebas18pet@gmail.com>
2022-12-21 17:44:25 -0500

    update

--- src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-    return (
-        <div className="loader">
-            <img src="/src/assets/react.svg" alt="LogoRact" />
-        </div>
-    )
-}
-
-export default Loader
