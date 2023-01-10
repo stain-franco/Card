4ab52e8590275fef4ef8e3e1d111045dc721b537
franco <sebas18pet@gmail.com>
2023-01-09 21:12:47 -0500

    entregable2

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,29 @@ import "./App.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Drinks from "./components/Drinks";
+import 'animate.css';
 
+function sortDrinks(drinks){
+  return drinks?.sort((a,b) =>{
+    if (a.strDrink > b.strDrink){
+      return 1;
+    } else if(a.strDrink<b.strDrink){
+      return -1;
+    } else {
+      return 0;
+    }
+  })
+}
 
 function App() {
 
   const [dataDrinks, setDataDrinks] = useState([]);
-  const [name , setName] = useState ("margarita")
+  const [name , setName] = useState ("")
 
   useEffect ( () =>{
     getData()
   }, [name])
+
   const getData = () =>{
     axios
     .get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
@@ -20,26 +33,27 @@ function App() {
       setDataDrinks(resp.data.drinks)
     })
     .catch(error => console.log(error))
+    setDataDrinks([]);
 
   }
   const searchDrink = (e) =>{
     e.preventDefault()
     setName(e.target[0].value.toLowerCase())
   }
-
   return (
     <div className="App">
       <form onSubmit={(e) => searchDrink(e)}>
-        <input type="text" placeholder="Search by name" />
+        <input type="text" placeholder=" Search by name" />
         <button type="submit">Search</button>
       </form>
-      {
-        dataDrinks.map( (drinks, index)=> 
+      <div className="container">
+      {  dataDrinks?.length ? 
+        sortDrinks(dataDrinks)?.map( (drinks, index)=> ( 
         <Drinks 
         key={`drinks-${index}`}
         data={drinks}
-        /> )
-      }
+        /> )): <h1>No hay coincidencias</h1> } 
+      </div>
     </div>
   );
 }
